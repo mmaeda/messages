a92913e8f0c753bbb202e1db3a09f5bc915239c5
Masayuki Maeda <mmae122@gmail.com>
2013-01-06 21:17:07 +0900

    Chapter23.2.1 引き続き。330ページ。サンプルとしてテキストフィールドを追加した。1つ目のサンプルが失敗になった。

--- spec/views/messages/new.html.erb_spec.rb
+++ spec/views/messages/new.html.erb_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe "messages/new.html.erb" do
   it "renders a form to create a message" do
-    assign(:message, mock_model("Message").as_new_record)
+    assign(
+      :message,
+      mock_model("Message").as_new_record)
     render
     rendered.should have_selector(
       "form",
@@ -10,6 +12,22 @@ describe "messages/new.html.erb" do
       :action => messages_path
     ) do |form|
       form.should have_selector("input", :type => "submit")
+    end
+  end
+
+  it "renders a text field for the message title" do
+    assign(
+      :message,
+      mock_model("Message", :title => "the title").as_new_record
+    )
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector(
+        "input",
+        :type => "text",
+        :name => "message[title]",
+        :value => "the title"
+      )
     end
   end
 end
