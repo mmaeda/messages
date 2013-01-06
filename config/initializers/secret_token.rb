99e22db1b7ba25252dbfe200732eeb2bd94a9fe1
Masayuki Maeda <mmae122@gmail.com>
2013-01-06 19:51:10 +0900

    初期状態。rails new messages とかGemfile編集とか。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messages::Application.config.secret_token = 'bc04da865323c12ec47003fb1fa3efc349f91d8b19af362e5bbdf0ddae9a32f22dfd22ea861e55de5656f03c925136dec0ebdc51aac4cea05b778bf9d7fdcdb4'
